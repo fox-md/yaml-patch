fb834fafb7df74f179608af8fb5329b1561fb7d5
G <fox@home.md>
2022-05-12 13:08:26 +0300

    Added logging using zerolog

--- cmd/yaml-patch/go.mod
+++ cmd/yaml-patch/go.mod
@@ -3,9 +3,10 @@ module yamlpatch
 go 1.17
 
 require (
-	github.com/fox-md/yaml-patch v0.0.14
+	github.com/fox-md/yaml-patch v0.0.15
 	github.com/onsi/ginkgo v1.16.5
-	github.com/onsi/gomega v1.10.1
+	github.com/onsi/gomega v1.17.0
+	github.com/rs/zerolog v1.26.1
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
@@ -14,9 +15,9 @@ require (
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/jessevdk/go-flags v1.5.0 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
--- cmd/yaml-patch/main.go
+++ cmd/yaml-patch/main.go
@@ -1,33 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"flag"
+	"strings"
+
 	yamlpatch "github.com/fox-md/yaml-patch"
+	"github.com/rs/zerolog"
 )
 
 func main() {
 	var filePath string
 	var patchLine string
 	var logLevel string
+	var outputFile string
 
 	flag.StringVar(&filePath, "file", "test.yaml", "file to patch")
+	flag.StringVar(&outputFile, "outfile", "patched.yaml", "file to output file")
 	flag.StringVar(&patchLine, "patch", "", "';' separated list of patches")
 	flag.StringVar(&logLevel, "loglevel", "info", "logging level (info|debug)")
 	flag.Parse()
+
+	if len(patchLine) == 0 {
+		log.Fatalf("patch not specified")
+	}
+
+	if strings.ToLower(logLevel) == "debug" {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	} else if strings.ToLower(logLevel) == "info" {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	} else {
+		log.Fatalf("log level must be set to 'info' or 'debug'")
+	}
 
 	yfile, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatalf("error reading file: %s", err)
 	}
-	
+
 	bs := yamlpatch.PatchFile(patchLine, yfile)
 
 	fmt.Println(string(bs))
 
-	err = ioutil.WriteFile("patched.yaml", bs, 0777)
+	err = ioutil.WriteFile(outputFile, bs, 0777)
 
 	if err != nil {
 		log.Fatal(err)
